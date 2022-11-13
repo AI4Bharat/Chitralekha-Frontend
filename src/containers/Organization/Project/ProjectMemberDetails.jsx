8f1801d60b4ff0bd816923651e2de15b998a3c98
Harsh Malviya <malviya.harsh12@gmail.com>
2022-11-13 23:53:46 +0530

    projects landing page api integration

---
+++ src/containers/Organization/Project/ProjectMemberDetails.jsx
@@ -0,0 +1,148 @@
+import React from "react";
+
+//Themes
+import { ThemeProvider } from "@mui/material";
+import tableTheme from "../../../theme/tableTheme";
+
+//Components
+import CustomButton from "../../../common/Button";
+import MUIDataTable from "mui-datatables";
+import { Link, useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
+import { roles } from "../../../utils/utils";
+
+const ProjectMemberDetails = ({ data }) => {
+  const { id } = useParams();
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const result = data.map((item) => {
+      return [
+        `${item.first_name} ${item.last_name}`,
+        item.username,
+        item.email,
+        item.availability_status,
+        roles.map((value) =>
+          value.id === item.role ? value.type : ""
+        ),
+        <Link
+          //   to={`/my-organization/${id}/project/${item.id}`}
+          style={{ textDecoration: "none" }}
+        >
+          <CustomButton sx={{ borderRadius: 2, marginRight: 2 }} label="View" />
+        </Link>,
+      ];
+    });
+
+    setTableData(result);
+  }, [data]);
+
+  const columns = [
+    {
+      name: "name",
+      label: "Name",
+      options: {
+        filter: false,
+        sort: false,
+        align: "center",
+        setCellHeaderProps: () => ({
+          style: { height: "30px", fontSize: "16px", padding: "16px" },
+        }),
+      },
+    },
+    {
+      name: "username",
+      label: "Username",
+      options: {
+        filter: false,
+        sort: false,
+        align: "center",
+        setCellHeaderProps: () => ({
+          style: { height: "30px", fontSize: "16px", padding: "16px" },
+        }),
+      },
+    },
+    {
+      name: "email",
+      label: "Email",
+      options: {
+        filter: false,
+        sort: false,
+        align: "center",
+        setCellHeaderProps: () => ({
+          style: { height: "30px", fontSize: "16px", padding: "16px" },
+        }),
+      },
+    },
+    {
+      name: "availability_status",
+      label: "Availability Status",
+      options: {
+        filter: false,
+        sort: false,
+        align: "center",
+        setCellHeaderProps: () => ({
+          style: { height: "30px", fontSize: "16px", padding: "16px" },
+        }),
+      },
+    },
+    {
+      name: "role",
+      label: "Role",
+      options: {
+        filter: false,
+        sort: false,
+        align: "center",
+        setCellHeaderProps: () => ({
+          style: { height: "30px", fontSize: "16px", padding: "16px" },
+        }),
+      },
+    },
+    {
+      name: "Action",
+      label: "Action",
+      options: {
+        filter: false,
+        sort: false,
+        align: "center",
+        setCellHeaderProps: () => ({
+          style: { height: "30px", fontSize: "16px", padding: "16px" },
+        }),
+      },
+    },
+  ];
+
+  const options = {
+    textLabels: {
+      body: {
+        noMatch: "No records",
+      },
+      toolbar: {
+        search: "Search",
+        viewColumns: "View Column",
+      },
+      pagination: { rowsPerPage: "Rows per page" },
+      options: { sortDirection: "desc" },
+    },
+    displaySelectToolbar: false,
+    fixedHeader: false,
+    filterType: "checkbox",
+    download: false,
+    print: false,
+    rowsPerPageOptions: [10, 25, 50, 100],
+    filter: false,
+    viewColumns: false,
+    selectableRows: "none",
+    search: false,
+    jumpToPage: true,
+  };
+
+  return (
+    <ThemeProvider theme={tableTheme}>
+      <MUIDataTable data={tableData} columns={columns} options={options} />
+    </ThemeProvider>
+  );
+};
+
+export default ProjectMemberDetails;
---
+++ src/containers/Organization/Project/VideoList.jsx
@@ -0,0 +1,145 @@
+import React from "react";
+
+//Themes
+import { ThemeProvider } from "@mui/material";
+import tableTheme from "../../../theme/tableTheme";
+
+//Components
+import CustomButton from "../../../common/Button";
+import MUIDataTable from "mui-datatables";
+import { Link, useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const VideoList = ({ data }) => {
+  const { id } = useParams();
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const result = data.map((item) => {
+      return [
+        item.id,
+        item.project_id,
+        item.name,
+        item.url,
+        item.duration,
+        <Link
+          //   to={`/my-organization/${id}/project/${item.id}`}
+          style={{ textDecoration: "none" }}
+        >
+          <CustomButton sx={{ borderRadius: 2, marginRight: 2 }} label="View" />
+        </Link>,
+      ];
+    });
+
+    setTableData(result);
+  }, [data]);
+
+  const columns = [
+    {
+      name: "id",
+      label: "Id",
+      options: {
+        filter: false,
+        sort: false,
+        align: "center",
+        setCellHeaderProps: () => ({
+          style: { height: "30px", fontSize: "16px", padding: "16px" },
+        }),
+      },
+    },
+    {
+      name: "projectId",
+      label: "Project Id",
+      options: {
+        filter: false,
+        sort: false,
+        align: "center",
+        setCellHeaderProps: () => ({
+          style: { height: "30px", fontSize: "16px", padding: "16px" },
+        }),
+      },
+    },
+    {
+      name: "name",
+      label: "Name",
+      options: {
+        filter: false,
+        sort: false,
+        align: "center",
+        setCellHeaderProps: () => ({
+          style: { height: "30px", fontSize: "16px", padding: "16px" },
+        }),
+      },
+    },
+    {
+      name: "url",
+      label: "URL",
+      options: {
+        filter: false,
+        sort: false,
+        align: "center",
+        setCellHeaderProps: () => ({
+          style: { height: "30px", fontSize: "16px", padding: "16px" },
+        }),
+      },
+    },
+    {
+      name: "duration",
+      label: "Duration",
+      options: {
+        filter: false,
+        sort: false,
+        align: "center",
+        setCellHeaderProps: () => ({
+          style: { height: "30px", fontSize: "16px", padding: "16px" },
+        }),
+      },
+    },
+    {
+      name: "Action",
+      label: "Action",
+      options: {
+        filter: false,
+        sort: false,
+        align: "center",
+        setCellHeaderProps: () => ({
+          style: { height: "30px", fontSize: "16px", padding: "16px" },
+        }),
+      },
+    },
+  ];
+
+  const options = {
+    textLabels: {
+      body: {
+        noMatch: "No records",
+      },
+      toolbar: {
+        search: "Search",
+        viewColumns: "View Column",
+      },
+      pagination: { rowsPerPage: "Rows per page" },
+      options: { sortDirection: "desc" },
+    },
+    displaySelectToolbar: false,
+    fixedHeader: false,
+    filterType: "checkbox",
+    download: false,
+    print: false,
+    rowsPerPageOptions: [10, 25, 50, 100],
+    filter: false,
+    viewColumns: false,
+    selectableRows: "none",
+    search: false,
+    jumpToPage: true,
+  };
+
+  return (
+    <ThemeProvider theme={tableTheme}>
+      <MUIDataTable data={tableData} columns={columns} options={options} />
+    </ThemeProvider>
+  );
+};
+
+export default VideoList;
