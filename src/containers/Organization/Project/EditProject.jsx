699aec6c20ec69f75ac3794767f15099e957b3da
harsh12-99 <malviya.harsh12@gmail.com>
2023-01-03 11:49:51 +0530

    updated project done

--- src/containers/Organization/Project/EditProject.jsx
+++ src/containers/Organization/Project/EditProject.jsx
@@ -8,12 +8,18 @@ import {
   InputLabel,
   Select,
   MenuItem,
+  Box,
+  Chip,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import CustomizedSnackbars from "../../../common/Snackbar";
+import FetchBulkTaskTypeAPI from "../../../redux/actions/api/Project/FetchBulkTaskTypes";
 import FetchProjectDetailsAPI from "../../../redux/actions/api/Project/FetchProjectDetails";
+import FetchSupportedLanguagesAPI from "../../../redux/actions/api/Project/FetchSupportedLanguages";
+import FetchTranscriptTypesAPI from "../../../redux/actions/api/Project/FetchTranscriptTypes";
+import FetchTranslationTypesAPI from "../../../redux/actions/api/Project/FetchTranslationTypes";
 import APITransport from "../../../redux/actions/apitransport/apitransport";
 
 const EditProject = () => {
@@ -21,6 +27,14 @@ const EditProject = () => {
   const dispatch = useDispatch();
 
   const projectInfo = useSelector((state) => state.getProjectDetails.data);
+  const supportedLanguages = useSelector(
+    (state) => state.getSupportedLanguages.data
+  );
+  const bulkTaskTypes = useSelector((state) => state.getBulkTaskTypes.data);
+  const transcriptTypes = useSelector((state) => state.getTranscriptTypes.data);
+  const translationTypes = useSelector(
+    (state) => state.getTranslationTypes.data
+  );
 
   const [snackbar, setSnackbarInfo] = useState({
     open: false,
@@ -28,14 +42,40 @@ const EditProject = () => {
     variant: "success",
   });
   const [projectDetails, setProjectDetails] = useState({});
+  const [managers, setManagers] = useState([]);
+  const [translationLanguage, setTranslationLanguage] = useState([]);
+  const [transcriptSourceType, setTranscriptSourceType] =
+    useState("MACHINE_GENERATED");
+  const [translationSourceType, setTranslationSourceType] =
+    useState("MACHINE_GENERATED");
+  const [defaultTask, setDefaultTask] = useState([]);
 
   useEffect(() => {
     const apiObj = new FetchProjectDetailsAPI(projectId);
     dispatch(APITransport(apiObj));
+
+    const langObj = new FetchSupportedLanguagesAPI();
+    dispatch(APITransport(langObj));
+
+    const bulkTaskObj = new FetchBulkTaskTypeAPI();
+    dispatch(APITransport(bulkTaskObj));
+
+    const transcriptObj = new FetchTranscriptTypesAPI();
+    dispatch(APITransport(transcriptObj));
+
+    const translationObj = new FetchTranslationTypesAPI();
+    dispatch(APITransport(translationObj));
   }, []);
 
   useEffect(() => {
-    setProjectDetails(projectInfo);
+    if (projectInfo && projectInfo.managers) {
+      setProjectDetails(projectInfo);
+      setManagers(projectInfo?.managers);
+      // setTranslationLanguage()
+      // setDefaultTask()
+      // setTranscriptSourceType()
+      // setTranslationSourceType()
+    }
   }, [projectInfo]);
 
   const handleFieldChange = (event) => {
@@ -102,54 +142,109 @@ const EditProject = () => {
               <FormControl fullWidth>
                 <InputLabel id="demo-simple-select-label">Manager</InputLabel>
                 <Select
+                  multiple
                   labelId="demo-simple-select-label"
                   id="demo-simple-select"
-                  value={12}
+                  value={managers}
                   label="Manager"
-                  onChange={() => {}}
+                  onChange={(e) => setManagers(e.target.value)}
+                  renderValue={(selected) => {
+                    return (
+                      <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                        {selected.map((value) => {
+                          return (
+                            <Chip
+                              key={value.id}
+                              label={`${value.first_name} ${value.last_name}`}
+                            />
+                          );
+                        })}
+                      </Box>
+                    );
+                  }}
                 >
                   {projectDetails?.managers?.map((item) => {
-                    return <MenuItem value={10}>{item.first_name}</MenuItem>;
+                    return (
+                      <MenuItem
+                        value={item}
+                      >{`${item.first_name} ${item.last_name}`}</MenuItem>
+                    );
                   })}
                 </Select>
               </FormControl>
             </Grid>
 
             <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-              <TextField
-                variant="outlined"
-                fullWidth
-                label="Default Transcript Source"
-                name="default_transcript_source"
-                InputLabelProps={{ shrink: true }}
-              />
+              <FormControl fullWidth>
+                <InputLabel id="transcription-source-type">
+                  Transcription Source
+                </InputLabel>
+                <Select
+                  labelId="transcription-source-type"
+                  id="transcription-source-type_select"
+                  value={transcriptSourceType}
+                  label="Transcription Source"
+                  onChange={(event) =>
+                    setTranscriptSourceType(event.target.value)
+                  }
+                >
+                  {transcriptTypes.map((item, index) => (
+                    <MenuItem key={index} value={item.value}>
+                      {item.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
             </Grid>
 
             <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-              <TextField
-                variant="outlined"
-                fullWidth
-                label="Default Translation Source"
-                name="default_translation_source"
-                value={projectDetails?.username}
-                onChange={handleFieldChange}
-                InputLabelProps={{ shrink: true }}
-              />
+              <FormControl fullWidth>
+                <InputLabel id="translation-source-type">
+                  Translation Source
+                </InputLabel>
+                <Select
+                  labelId="translation-source-type"
+                  id="translation-source-type_select"
+                  value={translationSourceType}
+                  label="Translation Source"
+                  onChange={(event) =>
+                    setTranslationSourceType(event.target.value)
+                  }
+                >
+                  {translationTypes.map((item, index) => (
+                    <MenuItem key={index} value={item.value}>
+                      {item.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
             </Grid>
 
             <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
               <FormControl fullWidth>
                 <InputLabel id="default_workflow">Default Workflow</InputLabel>
                 <Select
+                  multiple
                   labelId="default_workflow"
                   id="default_workflow_select"
-                  value={12}
+                  value={defaultTask}
                   label="Default Workflow"
-                  onChange={() => {}}
+                  onChange={(event) => setDefaultTask(event.target.value)}
+                  renderValue={(selected) => {
+                    return (
+                      <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                        {selected.map((value) => {
+                          return <Chip key={value.value} label={value.label} />;
+                        })}
+                      </Box>
+                    );
+                  }}
                 >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
+                  {bulkTaskTypes.map((item, index) => (
+                    <MenuItem key={index} value={item}>
+                      {item.label}
+                    </MenuItem>
+                  ))}
                 </Select>
               </FormControl>
             </Grid>
@@ -158,16 +253,28 @@ const EditProject = () => {
               <FormControl fullWidth>
                 <InputLabel id="targetlanguages">Target Languages</InputLabel>
                 <Select
+                  multiple
                   labelId="targetlanguages"
                   id="targetlanguages_select"
-                  value={12}
+                  value={translationLanguage}
                   name="targetlanguages"
                   label="Target Languages"
-                  onChange={() => {}}
+                  onChange={(e) => setTranslationLanguage(e.target.value)}
+                  renderValue={(selected) => {
+                    return (
+                      <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                        {selected.map((value) => {
+                          return <Chip key={value.value} label={value.label} />;
+                        })}
+                      </Box>
+                    );
+                  }}
                 >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
+                  {supportedLanguages?.map((item, index) => (
+                    <MenuItem key={index} value={item}>
+                      {item.label}
+                    </MenuItem>
+                  ))}
                 </Select>
               </FormControl>
             </Grid>
