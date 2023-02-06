7673b813dc9028850b853e859f0fb0f48047bb74
ganavikumarswamy <ganavi.kumarswamy@tarento.com>
2023-02-06 19:20:55 +0530

    .

--- src/common/HelpDialog.jsx
+++ src/common/HelpDialog.jsx
@@ -4,14 +4,15 @@ import CloseIcon from "@mui/icons-material/Close";
 import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
 import headerStyle from "../styles/header";
 import { translate } from "../config/localisation";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const HelpDialog = ({ openHelpDialog, handleClose, setOpenHelpDialog }) => {
   const classes = headerStyle();
   const [Addnewvideo, setAddnewvideo] = useState(false);
   const [EditingReviewTasks, setEditingReviewTasks] = useState(false);
-  const [AssignTasks,setAssignTasks] = useState(false);
-  const [EditTranscription,setEditTranscription] = useState(false);
-  const[Workflow,setWorkflow] = useState(false)
+  const [AssignTasks, setAssignTasks] = useState(false);
+  const [EditTranscription, setEditTranscription] = useState(false);
+  const [Workflow, setWorkflow] = useState(false);
 
   const handleaddnewvideo = () => {
     setAddnewvideo(true);
@@ -20,23 +21,24 @@ const HelpDialog = ({ openHelpDialog, handleClose, setOpenHelpDialog }) => {
   const handleEditingReviewTasks = () => {
     setEditingReviewTasks(true);
   };
-  const handleAssignTasks = () =>{
-    setAssignTasks(true)
-  }
+  const handleAssignTasks = () => {
+    setAssignTasks(true);
+  };
 
-  const handleEditTranscription = () =>{
-    setEditTranscription(true)
-  }
+  const handleEditTranscription = () => {
+    setEditTranscription(true);
+  };
 
-  const handleWorkflow = () =>{
-    setWorkflow(true)
-  }
-const handleback = ()=>{
-    setWorkflow(true)
-    setEditTranscription(true) 
-    setAssignTasks(true)
-}
-
+  const handleWorkflow = () => {
+    setWorkflow(true);
+  };
+  const handleArrowBackIcon = () => {
+    setWorkflow(false);
+    setEditTranscription(false);
+    setAssignTasks(false);
+    setEditingReviewTasks(false);
+    setAddnewvideo(false);
+  };
 
   return (
     <>
@@ -49,13 +51,11 @@ const handleback = ()=>{
       >
         <Grid sx={{ width: "500px", p: 2 }}>
           <Grid sx={{ display: "flex" }}>
-          <IconButton
-              aria-label="close"
-              onClick={handleback}
-            >
-              <CloseIcon />
+            <IconButton onClick={handleArrowBackIcon}>
+              <ArrowBackIcon />
             </IconButton>
-            <Typography sx={{ ml: 25 }} variant="h4" align="center">
+
+            <Typography sx={{ ml: 20 }} variant="h4" align="center">
               Help
             </Typography>
             <IconButton
@@ -66,76 +66,80 @@ const handleback = ()=>{
               <CloseIcon />
             </IconButton>
           </Grid>
-          {(Addnewvideo === false && EditingReviewTasks === false && AssignTasks === false && EditTranscription === false && Workflow === false) && (
-            <Grid sx={{ mt: 2, color: "black" }}>
-              <Typography variant="subtitle2" sx={{ fontSize: "20px" }}>
-                {translate("label.Popularhelpresources")}
-              </Typography>
-
-              <Grid
-                className={classes.HelpGridStyle}
-                onClick={handleaddnewvideo}
-              >
-                <div className={classes.ArticleIconStyle}>
-                  <ArticleOutlinedIcon color="primary" fontSize="medium" />
-                </div>
-
-                <Typography variant="subtitle1" align="center">
-                  {translate("label.addnewVideo")}
+          {Addnewvideo === false &&
+            EditingReviewTasks === false &&
+            AssignTasks === false &&
+            EditTranscription === false &&
+            Workflow === false && (
+              <Grid sx={{ mt: 2, color: "black" }}>
+                <Typography variant="subtitle2" sx={{ fontSize: "20px" }}>
+                  {translate("label.Popularhelpresources")}
                 </Typography>
-              </Grid>
-              <Grid
-                className={classes.HelpGridStyle}
-                onClick={handleEditingReviewTasks}
-              >
-                <div className={classes.ArticleIconStyle}>
-                  <ArticleOutlinedIcon color="primary" fontSize="medium" />
-                </div>
 
-                <Typography variant="subtitle1" align="center">
-                  {translate("label.Editing&ReviewTasks")}
-                </Typography>
-              </Grid>
-              <Grid
-                className={classes.HelpGridStyle}
-                onClick={handleAssignTasks}
-              >
-                <div className={classes.ArticleIconStyle}>
-                  <ArticleOutlinedIcon color="primary" fontSize="medium" />
-                </div>
+                <Grid
+                  className={classes.HelpGridStyle}
+                  onClick={handleaddnewvideo}
+                >
+                  <div className={classes.ArticleIconStyle}>
+                    <ArticleOutlinedIcon color="primary" fontSize="medium" />
+                  </div>
 
-                <Typography variant="subtitle1" align="center">
-                  {translate("label.assigntasks")}
-                </Typography>
-              </Grid>
-              <Grid
-                className={classes.HelpGridStyle}
-                onClick={handleEditTranscription}
-              >
-                <div className={classes.ArticleIconStyle}>
-                  <ArticleOutlinedIcon color="primary" fontSize="medium" />
-                </div>
+                  <Typography variant="subtitle1" align="center">
+                    {translate("label.addnewVideo")}
+                  </Typography>
+                </Grid>
+                <Grid
+                  className={classes.HelpGridStyle}
+                  onClick={handleEditingReviewTasks}
+                >
+                  <div className={classes.ArticleIconStyle}>
+                    <ArticleOutlinedIcon color="primary" fontSize="medium" />
+                  </div>
 
-                <Typography variant="subtitle1" align="center">
-                  {translate("label.editTranscription")}
-                </Typography>
-              </Grid>
-              <Grid
-                className={classes.HelpGridStyle}
-                onClick={handleWorkflow}
-              >
-                <div className={classes.ArticleIconStyle}>
-                  <ArticleOutlinedIcon color="primary" fontSize="medium" />
-                </div>
+                  <Typography variant="subtitle1" align="center">
+                    {translate("label.Editing&ReviewTasks")}
+                  </Typography>
+                </Grid>
+                <Grid
+                  className={classes.HelpGridStyle}
+                  onClick={handleAssignTasks}
+                >
+                  <div className={classes.ArticleIconStyle}>
+                    <ArticleOutlinedIcon color="primary" fontSize="medium" />
+                  </div>
 
-                <Typography variant="subtitle1" align="center">
-                  {translate("label.workflow")}
-                </Typography>
+                  <Typography variant="subtitle1" align="center">
+                    {translate("label.assigntasks")}
+                  </Typography>
+                </Grid>
+                <Grid
+                  className={classes.HelpGridStyle}
+                  onClick={handleEditTranscription}
+                >
+                  <div className={classes.ArticleIconStyle}>
+                    <ArticleOutlinedIcon color="primary" fontSize="medium" />
+                  </div>
+
+                  <Typography variant="subtitle1" align="center">
+                    {translate("label.editTranscription")}
+                  </Typography>
+                </Grid>
+                <Grid
+                  className={classes.HelpGridStyle}
+                  onClick={handleWorkflow}
+                >
+                  <div className={classes.ArticleIconStyle}>
+                    <ArticleOutlinedIcon color="primary" fontSize="medium" />
+                  </div>
+
+                  <Typography variant="subtitle1" align="center">
+                    {translate("label.workflow")}
+                  </Typography>
+                </Grid>
               </Grid>
-            </Grid>
-          )}
+            )}
         </Grid>
-        {Addnewvideo === true &&  (
+        {Addnewvideo === true && (
           <Grid className={classes.HelpGridStyle}>
             <div className={classes.ArticleIconStyle}>
               <ArticleOutlinedIcon color="primary" fontSize="medium" />
@@ -147,50 +151,52 @@ const handleback = ()=>{
           </Grid>
         )}
         <>
-        {EditingReviewTasks === true &&(
-         <Grid className={classes.HelpGridStyle}>
-            <div className={classes.ArticleIconStyle}>
-              <ArticleOutlinedIcon color="primary" fontSize="medium" />
-            </div>
+          {EditingReviewTasks === true && (
+            <Grid className={classes.HelpGridStyle}>
+              <div className={classes.ArticleIconStyle}>
+                <ArticleOutlinedIcon color="primary" fontSize="medium" />
+              </div>
 
-            <Typography variant="subtitle1" align="center">
-              some content here 2 ......................
-            </Typography>
-          </Grid>)}
-          {AssignTasks === true &&(
-         <Grid className={classes.HelpGridStyle}>
-            <div className={classes.ArticleIconStyle}>
-              <ArticleOutlinedIcon color="primary" fontSize="medium" />
-            </div>
+              <Typography variant="subtitle1" align="center">
+                some content here 2 ......................
+              </Typography>
+            </Grid>
+          )}
+          {AssignTasks === true && (
+            <Grid className={classes.HelpGridStyle}>
+              <div className={classes.ArticleIconStyle}>
+                <ArticleOutlinedIcon color="primary" fontSize="medium" />
+              </div>
 
-            <Typography variant="subtitle1" align="center">
-              some content here 3 ......................
-            </Typography>
-          </Grid>)}
-          {EditTranscription === true &&(
-         <Grid className={classes.HelpGridStyle}>
-            <div className={classes.ArticleIconStyle}>
-              <ArticleOutlinedIcon color="primary" fontSize="medium" />
-            </div>
+              <Typography variant="subtitle1" align="center">
+                some content here 3 ......................
+              </Typography>
+            </Grid>
+          )}
+          {EditTranscription === true && (
+            <Grid className={classes.HelpGridStyle}>
+              <div className={classes.ArticleIconStyle}>
+                <ArticleOutlinedIcon color="primary" fontSize="medium" />
+              </div>
 
-            <Typography variant="subtitle1" align="center">
-              some content here 4 ......................
-            </Typography>
-          </Grid>)}
-          {Workflow === true &&(
-         <Grid className={classes.HelpGridStyle}>
-            <div className={classes.ArticleIconStyle}>
-              <ArticleOutlinedIcon color="primary" fontSize="medium" />
-            </div>
+              <Typography variant="subtitle1" align="center">
+                some content here 4 ......................
+              </Typography>
+            </Grid>
+          )}
+          {Workflow === true && (
+            <Grid className={classes.HelpGridStyle}>
+              <div className={classes.ArticleIconStyle}>
+                <ArticleOutlinedIcon color="primary" fontSize="medium" />
+              </div>
 
-            <Typography variant="subtitle1" align="center">
-              some content here 5 ......................
-            </Typography>
-          </Grid>)}
+              <Typography variant="subtitle1" align="center">
+                some content here 5 ......................
+              </Typography>
+            </Grid>
+          )}
         </>
-        
       </Dialog>
-    
     </>
   );
 };
