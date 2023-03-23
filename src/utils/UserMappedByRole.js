827f2c03e217d3f51a31fb5406748bbc56a7f2fb
AlpanaMajhi <alpanamajhi001@gmail.com>
2023-03-23 17:10:22 +0530

    change color coding

--- src/utils/UserMappedByRole.js
+++ src/utils/UserMappedByRole.js
@@ -7,27 +7,27 @@ console.log(statusLabel,'statusLabel');
         return {
             name: statusLabel, name: "Complete", element: <Typography variant="caption"
                 sx={{
-                    p: 1, backgroundColor: "#fff", color: "rgb(56, 158, 13)" ,borderRadius: 2, fontWeight: 600
+                    p: 1, backgroundColor: "#fff", color:"green" ,borderRadius: 2, fontWeight: 600
                 }}>Complete</Typography>
         }
     } else if (statusLabel == "New") {
         return {
             name: statusLabel, name: "New", element: <Typography variant="caption"
                 sx={{
-                    p: 1,  backgroundColor: "#fff", color: "rgb(50, 100, 168)",
+                    p: 1,  backgroundColor: "#fff", color: "blue",
                     borderRadius: 2, fontWeight: 600
                 }}>New</Typography>
         }
     } else if (statusLabel == "In Progress") {
         return {
             name: statusLabel, name: "In Progress", element: <Typography variant="caption"
-                sx={{ p: 1, backgroundColor: "#fff", color: "rgb(255, 99, 71)", borderRadius: 2, fontWeight: 600,whiteSpace: "nowrap" }}>In Progress</Typography>
+                sx={{ p: 1, backgroundColor: "#fff", color: "maroon", borderRadius: 2, fontWeight: 600,whiteSpace: "nowrap" }}>In Progress</Typography>
         }
     }
     else if (statusLabel == "Selected Source") {
         return {
             name: statusLabel, name: "Selected Source", element: <Typography variant="caption"
-                sx={{ p: 1, backgroundColor: "#fff", color: "rgb(165, 105, 189)", borderRadius: 2, fontWeight: 600 ,whiteSpace: "nowrap"}} >Selected Source</Typography>
+                sx={{ p: 1, backgroundColor: "#fff", color: "purple", borderRadius: 2, fontWeight: 600 ,whiteSpace: "nowrap"}} >Selected Source</Typography>
         }
     } else if (statusLabel == "Post Process") {
       return {
@@ -39,7 +39,7 @@ console.log(statusLabel,'statusLabel');
             sx={{
               p: 1,
               backgroundColor: "#fff",
-              color: "rgb(234, 100, 168)",
+              color: "darkcyan",
               borderRadius: 2,
               fontWeight: 600,
               whiteSpace: "nowrap",
@@ -58,7 +58,7 @@ console.log(statusLabel,'statusLabel');
             sx={{
               p: 1,
               backgroundColor: "#fff",
-              color: "#f10000",
+              color: "#e60073",
               borderRadius: 2,
               fontWeight: 600,
               whiteSpace: "nowrap",
