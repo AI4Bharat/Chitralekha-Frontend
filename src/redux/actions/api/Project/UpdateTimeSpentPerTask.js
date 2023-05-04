9e762d3f7f9fc329101d9139b408b72968770faa
AlpanaMajhi <alpanamajhi001@gmail.com>
2023-05-04 15:17:37 +0530

    time spent changes

--- src/redux/actions/api/Project/UpdateTimeSpentPerTask.js
+++ src/redux/actions/api/Project/UpdateTimeSpentPerTask.js
@@ -5,11 +5,11 @@ import ENDPOINTS from "../../../../config/apiendpoint";
 import C from "../../../constants";
 
 export default class UpdateTimeSpentPerTask extends API {
-  constructor(taskId = "", timeSpent = "", timeout = 2000) {
+  constructor(taskId = "", time_spent = "", timeout = 2000) {
     super("PATCH", timeout, false);
     this.type = C.UPDATE_TIME_SPENT;
     this.taskId = taskId;
-    this.timeSpent = timeSpent;
+    this.time_spent = time_spent;
     this.endpoint = `${super.apiEndPointAuto()}${ENDPOINTS.task}${this.taskId}/update_time_spent/`;
   }
 
@@ -26,7 +26,7 @@ export default class UpdateTimeSpentPerTask extends API {
 
   getBody() {
     return {
-        timeSpent: this.timeSpent
+      time_spent: this.time_spent
     };
   }
 
