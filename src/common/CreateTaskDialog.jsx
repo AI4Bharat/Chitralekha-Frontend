973e40de043afa61adf7f5803ca48e063d0cc4e6
harsh12-99 <malviya.harsh12@gmail.com>
2023-07-26 18:34:37 +0530

    displayed warning if more than 10 videos are selected

--- src/common/CreateTaskDialog.jsx
+++ src/common/CreateTaskDialog.jsx
@@ -26,16 +26,17 @@ import {
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import CloseIcon from "@mui/icons-material/Close";
 import Loader from "./Spinner";
+import WarningIcon from "@mui/icons-material/Warning";
 
 //APIs
 import {
   APITransport,
   FetchAllowedTasksAPI,
-  FetchSupportedBulkTaskTypeAPI,
   FetchPriorityTypesAPI,
   FetchProjectMembersAPI,
   FetchSupportedLanguagesAPI,
   FetchTaskTypeAPI,
+  FetchBulkTaskTypeAPI,
 } from "redux/actions";
 
 const CreateTaskDialog = ({
@@ -60,9 +61,7 @@ const CreateTaskDialog = ({
   const voiceoverLanguage = useSelector(
     (state) => state.getSupportedLanguages.voiceoverLanguage
   );
-  const bulkTaskTypes = useSelector(
-    (state) => state.getSupportedBulkTaskTypes.data
-  );
+  const bulkTaskTypes = useSelector((state) => state.getBulkTaskTypes.data);
 
   const [taskType, setTaskType] = useState("");
   const [description, setDescription] = useState("");
@@ -72,6 +71,7 @@ const CreateTaskDialog = ({
   const [date, setDate] = useState(moment().format());
   const [allowedTaskType, setAllowedTaskType] = useState("");
   const [showAllowedTaskList, setShowAllowedTaskList] = useState(false);
+  const [showLimitWarning, setShowLimitWarning] = useState(false);
 
   useEffect(() => {
     const taskObj = new FetchTaskTypeAPI();
@@ -80,7 +80,7 @@ const CreateTaskDialog = ({
     const priorityTypesObj = new FetchPriorityTypesAPI();
     dispatch(APITransport(priorityTypesObj));
 
-    const bulkTaskObj = new FetchSupportedBulkTaskTypeAPI();
+    const bulkTaskObj = new FetchBulkTaskTypeAPI();
     dispatch(APITransport(bulkTaskObj));
 
     // eslint-disable-next-line
@@ -116,6 +116,12 @@ const CreateTaskDialog = ({
     } = event;
 
     setTaskType(value);
+
+    if (videoDetails.length > 10) {
+      value.includes("TRANSCRIPTION_EDIT")
+        ? setShowLimitWarning(true)
+        : setShowLimitWarning(false);
+    }
 
     if (isBulk && value.includes("TRANSCRIPTION")) {
       const obj = new FetchProjectMembersAPI(projectId, value, "", "");
@@ -248,6 +254,21 @@ const CreateTaskDialog = ({
           justifyContent="center"
           alignItems="center"
         >
+          {showLimitWarning && (
+            <Box display="flex" alignItems="center" marginRight="auto">
+              <WarningIcon color="error" />
+              <Typography
+                variant="body2"
+                fontWeight="bold"
+                color="error"
+                marginLeft="5px"
+              >
+                A maximum of 10 Transcription Edit tasks can be created
+                simultaneously.
+              </Typography>
+            </Box>
+          )}
+
           <Box width={"100%"} sx={{ mt: 3 }}>
             <FormControl fullWidth>
               <InputLabel id="select-task">Select Task Type*</InputLabel>
@@ -413,7 +434,7 @@ const CreateTaskDialog = ({
           autoFocus
           variant="contained"
           sx={{ borderRadius: 2 }}
-          disabled={isBulk ? false : disableBtn()}
+          disabled={isBulk ? showLimitWarning : disableBtn()}
           onClick={() => submitHandler()}
         >
           Create Task{" "}
