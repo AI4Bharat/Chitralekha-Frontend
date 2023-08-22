b8ad6d40b02ce0a3a030d42db77e80cc1fd01b27
AlpanaMajhi <alpanamajhi001@gmail.com>
2023-08-22 14:05:29 +0530

    landing page changes

--- src/containers/intro/Partners.jsx
+++ src/containers/intro/Partners.jsx
@@ -27,12 +27,12 @@ const Partners = () => {
             direction="row"
             alignItems="center"
             justifyContent="center"
-            sx={{
-              mt: 10,
-              mb: 10,
-              display: "flex",
-              justifyContent: "space-between",
-            }}
+            // sx={{
+            //   mt: 10,
+            //   mb: 10,
+            //   display: "flex",
+            //   justifyContent: "space-between",
+            // }}
           >
             {partnerData?.map((el, i) => (
               <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
