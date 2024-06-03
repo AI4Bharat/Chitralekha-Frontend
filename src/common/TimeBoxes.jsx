7298b4d8be92d058f2e4ec19534a05a450387a4e
kartikvirendrar <kartikrajput357@gmail.com>
2024-06-03 10:03:05 +0530

    make timeboxes readonly

--- src/common/TimeBoxes.jsx
+++ src/common/TimeBoxes.jsx
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 import { ProjectStyle } from "styles";
 import { TextField } from "@mui/material";
 
-const TimeBoxes = ({ handleTimeChange, time, index, type }) => {
+const TimeBoxes = ({ handleTimeChange, time, index, type, readOnly }) => {
   const classes = ProjectStyle();
 
   return (
@@ -15,6 +15,7 @@ const TimeBoxes = ({ handleTimeChange, time, index, type }) => {
         value={time.split(":")[0]}
         onFocus={(event) => event.target.select()}
         className={classes.timeInputBox}
+        InputProps={{ readOnly:readOnly }}
         style={{
           // paddingLeft: "10px",
           // marginLeft: type === "endTime" ? "auto" : "",
@@ -33,13 +34,14 @@ const TimeBoxes = ({ handleTimeChange, time, index, type }) => {
         value={time.split(":")[1]}
         className={classes.timeInputBox}
         onFocus={(event) => event.target.select()}
-        InputProps={{ inputProps: { min: 0, max: 100 } }}
+        InputProps={{ inputProps: { min: 0, max: 100 }, readOnly:readOnly }}
         onChange={(event) =>
           handleTimeChange(event.target.value, index, type, "minutes")
         }
       />
 
       <TextField
+        
         variant="standard"
         value={":"}
         style={{ width: "1ch" }}
@@ -50,7 +52,7 @@ const TimeBoxes = ({ handleTimeChange, time, index, type }) => {
         variant="standard"
         value={time.split(":")[2].split(".")[0]}
         onFocus={(event) => event.target.select()}
-        InputProps={{ inputProps: { min: 0, max: 100 } }}
+        InputProps={{ inputProps: { min: 0, max: 100 }, readOnly:readOnly }}
         className={classes.timeInputBox}
         onChange={(event) =>
           handleTimeChange(event.target.value, index, type, "seconds")
@@ -73,7 +75,7 @@ const TimeBoxes = ({ handleTimeChange, time, index, type }) => {
         style={{width: "3ch"}}
         // style={{ width: "20%", paddingRight: "10px" }}
         onFocus={(event) => event.target.select()}
-        InputProps={{ inputProps: { min: 0, max: 999 } }}
+        InputProps={{ inputProps: { min: 0, max: 999 }, readOnly:readOnly }}
         className={classes.timeInputBox}
         onChange={(event) =>
           handleTimeChange(event.target.value, index, type, "miliseconds")
