cfefdd760da4a9f42fbf9882bffa2dd0542faaf6
kartikvirendrar <kartikrajput357@gmail.com>
2024-08-06 11:12:01 +0530

    added 1000 rows option for reports

--- src/containers/Organization/Project/ProjectReport.jsx
+++ src/containers/Organization/Project/ProjectReport.jsx
@@ -248,6 +248,7 @@ const ProjectReport = () => {
       rowsPerPage: limit,
       count: total_count,
       customToolbar: renderToolBar,
+      rowsPerPageOptions: [10, 25, 50, 100, 1000],
       onTableChange: (action, tableState) => {
         switch (action) {
           case "changePage":
