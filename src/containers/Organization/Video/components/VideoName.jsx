46a181002635f95f84d5aca0db97f51ed6d0f037
kartikvirendrar <kartikrajput357@gmail.com>
2024-08-12 17:55:49 +0530

    minor fixes for youtube player

--- src/containers/Organization/Video/components/VideoName.jsx
+++ src/containers/Organization/Video/components/VideoName.jsx
@@ -44,7 +44,7 @@ const VideoName = ({
           className={classes.videoName}
           style={fullscreenVideo ? { color: "white" } : {}}
         >
-          {videoDetails?.video?.name ? videoDetails?.video?.name : player?.playerInfo?.videoData?.title && player?.playerInfo?.videoData?.title}
+          {videoDetails?.video?.name && videoDetails?.direct_video_url !== "" ? videoDetails?.video?.name : player?.playerInfo?.videoData?.title && player?.playerInfo?.videoData?.title}
         </Typography>
       </Tooltip>
 
